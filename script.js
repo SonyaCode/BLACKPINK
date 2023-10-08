e1b9574350bec28c8d37d9cba3188b8daf5cb928
SonyaZheng1 <11007617-SonyaZheng1@users.noreply.replit.com>
2023-10-08 15:28:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function check() {
+  document.write("Answers: <br> Main rapper: Jennie <br> Main vocalist: Rosé <br> Main dancer: Lisa <br> Visual: Jisoo <br> Lead vocalists: Jisoo, Jennie <br> Lead dancer: Rosé <br> Sub-vocalist: Lisa")
+}
+function check1() {
+  document.write("Answers: <br> Jisoo <br> Lisa <br> Jennie <br> Rosé <br> Jennie <br> Rosé <br> Jisoo <br> Lisa")
+}
